ca3f449d1029de19ebd4f0f67a7c87551e6e02da
Donald Futers <donald.futers@gmail.com>
2024-11-12 12:08:26 +0000

    CI/CD test push

---
+++ src/pipeline_tester.py
@@ -0,0 +1,2 @@
+def sample_func(num1, num2):
+  return num1 * num2
---
+++ test/test_pipeline_tester.py
@@ -0,0 +1,5 @@
+from src.pipeline_tester import sample_func
+
+def test_returns_6():
+  output = sample_func(2, 3)
+  assert output == 6
