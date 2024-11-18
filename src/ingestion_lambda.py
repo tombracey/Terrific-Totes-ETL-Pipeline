14c3f4db80447c32f1d964e901df8323aca1cd91
Tom Bracey <tombracey22@gmail.com>
2024-11-18 15:31:24 +0000

    Finished refactoring get_data

--- src/ingestion_lambda.py
+++ src/ingestion_lambda.py
@@ -2,7 +2,7 @@ import json
 import datetime
 import boto3
 import os
-import pg8000.native
+from pg8000.native import Connection, identifier
 from dotenv import load_dotenv
 
 BUCKET_NAME = os.environ["INGESTION_BUCKET_NAME"]
@@ -38,74 +38,8 @@ def get_data(db, last_update):
 
     for table in tables:
 
-        query = db.run(f"SELECT * FROM {table} WHERE last_updated >= :last_update;", last_update=last_update)
+        query = db.run(f"SELECT * FROM {identifier(table)} WHERE last_updated >= :last_update;", last_update=last_update)
         data[table] = (query, [col["name"] for col in db.columns])
-
-    # counterparty = db.run(
-    #     "SELECT * FROM counterparty WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["counterparty"] = (counterparty, [col["name"] for col in db.columns])
-
-    # currency = db.run(
-    #     "SELECT * FROM currency WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["currency"] = (currency, [col["name"] for col in db.columns])
-
-    # department = db.run(
-    #     "SELECT * FROM department WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["department"] = (department, [col["name"] for col in db.columns])
-
-    # design = db.run(
-    #     "SELECT * FROM design WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["design"] = (design, [col["name"] for col in db.columns])
-
-    # staff = db.run(
-    #     "SELECT * FROM staff WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["staff"] = (staff, [col["name"] for col in db.columns])
-
-    # sales_order = db.run(
-    #     "SELECT * FROM sales_order WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["sales_order"] = (sales_order, [col["name"] for col in db.columns])
-
-    # address = db.run(
-    #     "SELECT * FROM address WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["address"] = (address, [col["name"] for col in db.columns])
-
-    # payment = db.run(
-    #     "SELECT * FROM payment WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["payment"] = (payment, [col["name"] for col in db.columns])
-
-    # purchase_order = db.run(
-    #     "SELECT * FROM purchase_order WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["purchase_order"] = (purchase_order, [col["name"] for col in db.columns])
-
-    # payment_type = db.run(
-    #     "SELECT * FROM payment_type WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["payment_type"] = (payment_type, [col["name"] for col in db.columns])
-
-    # transaction = db.run(
-    #     "SELECT * FROM transaction WHERE last_updated >= :last_update;",
-    #     last_update=last_update,
-    # )
-    # data["transaction"] = (transaction, [col["name"] for col in db.columns])
 
     return data
 
