86f826b6b42eab0f5f437ce5bbfa90b235e324f6
contiele1 <conti.daniele.13@gmail.com>
2024-11-19 13:53:50 +0000

    working on state machine

--- terraform/state_machine.tf
+++ terraform/state_machine.tf
@@ -1,10 +1,10 @@
 
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name_prefix = "var.state_machine_name"
-  definition = templatefile("${path.module}/statemachine/state_machine.json",
+  name_prefix = var.state_machine_name
+  definition = templatefile("${path.module}/../statemachine/state_machine.json",
     { aws_region        = data.aws_region.current.name,
       aws_account_num   = data.aws_caller_identity.current.account_id,
       function_name     = var.ingestion_lambda_name
     })
-  role_arn = aws_iam_role.step_function_role.arn
+  role_arn = aws_iam_role.state_machine_role.arn
 }
