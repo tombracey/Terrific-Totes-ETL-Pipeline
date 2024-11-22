1bee55f77f0ea431129f9cafd8507c4344a417c3
Donald Futers <donald.futers@gmail.com>
2024-11-22 09:47:59 +0000

    UTILS/FETCH: pass third and final test on fetch_latest_row_versions(); complete implementation

--- test/test_fetch_latest_row_versions.py
+++ test/test_fetch_latest_row_versions.py
@@ -3,6 +3,7 @@ import pytest, os, boto3
 import pandas as pd
 from moto import mock_aws
 
+
 @pytest.fixture(scope="function")
 def aws_credentials():
     """Mocked AWS Credentials for moto"""
@@ -24,9 +25,7 @@ def s3_client(aws_credentials):
 def s3_bucket(s3_client):
     s3_client.create_bucket(
         Bucket="test_bucket",
-        CreateBucketConfiguration={
-            "LocationConstraint": "eu-west-2"
-        }
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
     yield s3_client
 
@@ -35,9 +34,11 @@ def test_single_file_with_correct_id_returns_expected_single_row_dataframe(s3_bu
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 13_32_38.364280.json",
-        Key="sales_order/2024-11-21 13_32_38.364280.json"
+        Key="sales_order/2024-11-21 13_32_38.364280.json",
     )
-    output_df = fetch_latest_row_versions(s3_bucket, "test_bucket", "sales_order", [11283])
+    output_df = fetch_latest_row_versions(
+        s3_bucket, "test_bucket", "sales_order", [11283]
+    )
     assert isinstance(output_df, pd.DataFrame)
     assert len(output_df.index) == 1
     assert output_df.loc[0, "sales_order_id"] == 11283
@@ -54,24 +55,28 @@ def test_single_file_with_correct_id_returns_expected_single_row_dataframe(s3_bu
     assert output_df.loc[0, "agreed_delivery_location_id"] == 15
 
 
-def test_seeking_one_id_in_multiple_matching_files_returns_expected_single_row_dataframe(s3_bucket):
+def test_seeking_one_id_in_multiple_matching_files_returns_expected_single_row_dataframe(
+    s3_bucket,
+):
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 15_47_38.454675.json",
-        Key="sales_order/2024-11-21 15_47_38.454675.json"
+        Key="sales_order/2024-11-21 15_47_38.454675.json",
     )
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 13_32_38.364280.json",
-        Key="sales_order/2024-11-21 13_32_38.364280.json"
+        Key="sales_order/2024-11-21 13_32_38.364280.json",
     )
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 16_02_38.340563.json",
-        Key="sales_order/2024-11-21 16_02_38.340563.json"
+        Key="sales_order/2024-11-21 16_02_38.340563.json",
     )
 
-    output_df = fetch_latest_row_versions(s3_bucket, "test_bucket", "sales_order", [11283])
+    output_df = fetch_latest_row_versions(
+        s3_bucket, "test_bucket", "sales_order", [11283]
+    )
 
     assert len(output_df.index) == 1
     assert output_df.loc[0, "sales_order_id"] == 11283
@@ -92,30 +97,39 @@ def test_seeking_multiple_ids_in_multiple_files_returns_expected_dataframe(s3_bu
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 15_47_38.454675.json",
-        Key="sales_order/2024-11-21 15_47_38.454675.json"
+        Key="sales_order/2024-11-21 15_47_38.454675.json",
     )
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 13_32_38.364280.json",
-        Key="sales_order/2024-11-21 13_32_38.364280.json"
+        Key="sales_order/2024-11-21 13_32_38.364280.json",
     )
     s3_bucket.upload_file(
         Bucket="test_bucket",
         Filename="test/test_data/sales_order/2024-11-21 16_02_38.340563.json",
-        Key="sales_order/2024-11-21 16_02_38.340563.json"
+        Key="sales_order/2024-11-21 16_02_38.340563.json",
     )
 
-    output_df = fetch_latest_row_versions(s3_bucket, "test_bucket", "sales_order", [11283, 11285, 11291])
+    output_df = fetch_latest_row_versions(
+        s3_bucket, "test_bucket", "sales_order", [11283, 11285, 11291]
+    )
 
     assert len(output_df.index) == 3
 
-    print(output_df.query("sales_order_id == 11283").loc[0, "last_updated"])
+    index_1 = output_df.query("sales_order_id == 11283").index[0]
+    last_updated_1 = output_df.query("sales_order_id == 11283").loc[
+        index_1, "last_updated"
+    ]
+    assert last_updated_1 == "2024-11-21 15:54:09.995000"
 
-    assert output_df.loc[0, "sales_order_id"] == 11283
-    assert output_df.loc[0, "last_updated"] == "2024-11-21 15:54:09.995000"
+    index_2 = output_df.query("sales_order_id == 11285").index[0]
+    last_updated_2 = output_df.query("sales_order_id == 11285").loc[
+        index_2, "last_updated"
+    ]
+    assert last_updated_2 == "2024-11-21 13:32:09.809000"
 
-    assert output_df.loc[1, "sales_order_id"] == 11285
-    assert output_df.loc[1, "last_updated"] == "2024-11-21 13:32:09.809000"
-
-    assert output_df.loc[2, "sales_order_id"] == 11291
-    assert output_df.loc[2, "last_updated"] == "2024-11-21 15:40:10.078000"
+    index_3 = output_df.query("sales_order_id == 11291").index[0]
+    last_updated_3 = output_df.query("sales_order_id == 11291").loc[
+        index_3, "last_updated"
+    ]
+    assert last_updated_3 == "2024-11-21 15:40:10.078000"
