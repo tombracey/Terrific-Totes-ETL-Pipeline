8ce3c244c9e6c87f5bf797fc32812fbd3a88f096
Tom Bracey <tombracey22@gmail.com>
2024-11-22 11:35:04 +0000

    Added if statement to create tmp folder if necessary

--- src/utils/df_to_parquet_in_s3.py
+++ src/utils/df_to_parquet_in_s3.py
@@ -6,7 +6,8 @@ import os
 # pyarrow
 
 def df_to_parquet_in_s3(client, df, bucket_name, folder, file_name):
-    # os.mkdir("tmp")
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
     df.to_parquet(f"./tmp/{file_name}.parquet") 
 
     client.upload_file(f"./tmp/{file_name}.parquet", bucket_name, f"{folder}/{file_name}.parquet")
