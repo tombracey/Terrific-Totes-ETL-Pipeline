176b711562d716fb248ef4da0ed16a6dbcc65a07
Donald Futers <donald.futers@gmail.com>
2024-11-24 19:21:43 +0000

    LAMBDA/PROCESSING: add tests on make_exclusion_list(), missing from EOD Friday commit

---
+++ test/test_make_exclusion_list.py
@@ -0,0 +1,50 @@
+from src.processing_lambda import make_exclusion_list
+import pytest, os, boto3
+from moto import mock_aws
+
+
+# make_exclusion_list(s3_client, table_name, last_checked_time)
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto"""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture
+def s3_client(aws_credentials):
+    with mock_aws():
+        s3 = boto3.client("s3")
+        yield s3
+
+
+@pytest.fixture
+def s3_bucket(s3_client):
+    s3_client.create_bucket(
+        Bucket="test_bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+    yield s3_client
+
+
+def test_make_exclusion_list_fetches_updated_ids_from_update_packet(s3_bucket):
+    s3_bucket.upload_file(
+        Bucket="test_bucket",
+        Filename="test/test_data/staff/2024-11-20 15_22_10.531518.json",
+        Key="staff/2024-11-20 15_22_10.531518.json"
+    )
+    s3_bucket.upload_file(
+        Bucket="test_bucket",
+        Filename="test/test_data/staff/2024-11-21 09_38_15.221234.json",
+        Key="staff/2024-11-21 09_38_15.221234.json"
+    )
+
+    test_exclusion_list = make_exclusion_list(s3_bucket, "test_bucket", "staff", "2024-11-21 09_38_15.221234")
+
+    assert isinstance(test_exclusion_list, list)
+    assert test_exclusion_list == [1, 16]
