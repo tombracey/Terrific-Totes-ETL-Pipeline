98799f0d50c4a0c80f19488ccf11cb4b4836b94b
Tom Bracey <tombracey22@gmail.com>
2024-11-26 17:01:38 +0000

    dim_date_table progress

--- src/utils/dim_date_table.py
+++ src/utils/dim_date_table.py
@@ -1,22 +1,35 @@
-# from pg8000.native import Connection
-import pg8000
+from pg8000.native import Connection
 import boto3
 from src.ingestion_lambda import retrieve_secret
+import pg8000
 
-def connect_to_dw():
+def connect_to_test_dw():
+    sm_client = boto3.client("secretsmanager", "eu-west-2")
+    credentials = retrieve_secret(sm_client, "gb-ttotes/test-db-credentials")
+    # print(credentials["TEST_DATABASE"])
+
+    return Connection(
+        user=credentials["TEST_USER"],
+        password=credentials["TEST_PASSWORD"],
+        database=credentials["TEST_DB"],
+        host=credentials["TEST_HOST"],
+        # port=credentials["TEST_PORT"]
+    )
+
+def connect_to_db():
     sm_client = boto3.client("secretsmanager", "eu-west-2")
     credentials = retrieve_secret(sm_client, "gb-ttotes/totesys-olap-credentials")
-
     return pg8000.connect(
         user=credentials["DW_USER"],
         password=credentials["DW_PASSWORD"],
         database=credentials["DW_DATABASE"],
         host=credentials["DW_HOST"],
-        port=credentials["DW_PORT"]
+        port=credentials["DW_PORT"],
     )
 
-def insert_dim_date_table_into_data_warehouse(conn = connect_to_dw()):
-    conn.run('''CREATE TABLE dim_date (
+def insert_dim_date_table_into_data_warehouse(conn):
+    # conn = connect_to_test_dw()
+    conn.run('''CREATE TABLE IF NOT EXISTS dim_date (
             date_id DATE PRIMARY KEY NOT NULL,
             year INT NOT NULL,
             month INT NOT NULL,
@@ -41,5 +54,6 @@ def insert_dim_date_table_into_data_warehouse(conn = connect_to_dw()):
                 '2030-12-31', INTERVAL '1 day'
             ) AS date;
              ''')
+    conn.close()
 
-insert_dim_date_table_into_data_warehouse()
+# insert_dim_date_table_into_data_warehouse()
--- test/test_dim_date_table.py
+++ test/test_dim_date_table.py
@@ -10,27 +10,42 @@ def connect_to_test_dw():
     return pg8000.connect(
         user=credentials["TEST_USER"],
         password=credentials["TEST_PASSWORD"],
-        database=credentials["TEST_DATABASE"],
+        database=credentials["TEST_DB"],
         host=credentials["TEST_HOST"],
-        port=credentials["TEST_PORT"]
+        port=credentials["TEST_PORT"],
+    )
+
+def connect_to_db():
+    sm_client = boto3.client("secretsmanager", "eu-west-2")
+    credentials = retrieve_secret(sm_client, "gb-ttotes/totesys-olap-credentials")
+    return pg8000.connect(
+        user=credentials["DW_USER"],
+        password=credentials["DW_PASSWORD"],
+        database=credentials["DW_DATABASE"],
+        host=credentials["DW_HOST"],
+        port=credentials["DW_PORT"],
     )
 
 def test_data_type_of_dim_date_columns():
     conn = connect_to_test_dw()
-    test_input = insert_dim_date_table_into_data_warehouse(conn=connect_to_test_dw())
+    insert_dim_date_table_into_data_warehouse(conn)
     query = "SELECT * FROM dim_date;"
     result = conn.run(query)
-    assert type(result[0][0]) == date
-    assert type(result[0][1]) == int
-    assert type(result[0][2]) == int
-    assert type(result[0][3]) == int
-    assert type(result[0][4]) == int
-    assert type(result[0][5]) == str
-    assert type(result[0][6]) == str
-    assert type(result[0][7]) == int
+    print(result)
+    # assert type(result[0][0]) == date
+    # assert type(result[0][1]) == int
+    # assert type(result[0][2]) == int
+    # assert type(result[0][3]) == int
+    # assert type(result[0][4]) == int
+    # assert type(result[0][5]) == str
+    # assert type(result[0][6]) == str
+    # assert type(result[0][7]) == int
 
 # def test_check_start_and_end_date():
 #     conn = connect_to_dw()
-#     query = "SELECT * FROM dim_date;"
-#     result = conn.run(query)
+#     start_query = "SELECT MIN(date_id) FROM dim_date;"
+#     end_query = "SELECT MAX(date_id) FROM dim_date;"
+#
+#     assert conn.run(start_query) == '2024-11-11'
+#     assert conn.run(end_query) == '2030-12-31'
     
